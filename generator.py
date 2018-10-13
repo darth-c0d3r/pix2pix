58dcbcceafa1c3a0411bab4bfedb0ee746f66627
darth-c0d3r <gurparkashsohi@gmail.com>
2018-10-13 12:08:42 +0530

    basic generator discriminator

---
+++ generator.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.nn.parameter import Parameter
+from collections import OrderedDict
+import numpy as np
+import torch.nn.functional as F
+
+class EncoderDecoderGenerator(nn.Module):
+
+	def __init__(self, size, conv):
+		super(c_nn, self).__init__()
+
+		# model parameters
+		self.input_size = size
+		self.kernel_size = 3
+		self.padding = 1
+		self.dropout = 0.5
+		self.leaky_relu_slope = 0.2
+
+		self.batchnorm_layers = nn.ModuleList()
+		self.conv_layers = nn.ModuleList()
+		self.deconv_layers = nn.ModuleList()
+
+		for i in range(len(conv)-1):
+			self.conv_layers.append(nn.Conv2d(conv[i], conv[i+1], kernel_size=self.kernel_size, padding=self.padding))
+			self.batchnorm_layers.append(nn.BatchNorm2d(conv[i+1]))
+
+		conv.reverse()
+		
+		for i in range(len(conv)-1):
+			self.conv_layers.append(nn.ConvTranspose2d(conv[i], conv[i+1], kernel_size=self.kernel_size, padding=self.padding))
+			self.batchnorm_layers.append(nn.BatchNorm2d(conv[i+1]))
+
+
+	def forward(self, x):
+
+		batchnorm_index = 0
+
+		# convolutional layers
+		for conv_layer in self.conv_layers:
+			x = F.relu(conv_layer(x))
+			x = self.batchnorm_layers[batchnorm_index](x)
+			batchnorm_index += 1
+
+		# deconvolutional layers
+		for deconv_layer in self.deconv_layers:
+			x = F.relu(deconv_layer(x))
+			x = self.batchnorm_layers[batchnorm_index](x)
+			batchnorm_index += 1
+
+		x = F.tanh(x)
+
+		return x
