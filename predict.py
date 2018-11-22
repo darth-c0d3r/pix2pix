69e53b5ab5a70bb12e7c76e7db680718e0a7d564
rathi22 <rajatrathidgr81@gmail.com>
2018-11-22 15:44:36 +0530

    Updated predict.py

--- predict.py
+++ predict.py
@@ -2,23 +2,39 @@ import sys
 import torch
 import torchvision
 import numpy as np
-
-num_iter = 100
-total_loss = float(0)
+from PIL import Image
+import cv2
+import matplotlib.pyplot as plt
+import os
 
 cuda = 1
 gpu_id = 0
 device = torch.device("cuda:"+str(gpu_id) if torch.cuda.is_available() and cuda == 1 else "cpu")
-print(device)
+print("Device:", device)
 
-generator = torch.load(sys.argv[1]).to(device)
 
-for _ in range(num_iter):
-	with torch.no_grad():
-		tensor = torch.FloatTensor(1,1,5,5).uniform_(-1,0).to(device)
-		output = generator(tensor)
-		# print(tensor)
-		total_loss += (abs(output+tensor)).sum()/float(np.prod(tensor.size()))
+folder = sys.argv[1]
+task = sys.argv[2]
 
-total_loss /= float(num_iter)
-print(total_loss.item())
+generator = torch.load("saved_models/generator_model_"+task+"_62.pt", map_location='cpu').to(device)
+generator.eval()
+root_dir_input = "datasets/"+folder+"/eval_"+task+"/input/"
+root_dir_output = "datasets/"+folder+"/eval_"+task+"/output/"
+files = os.listdir(root_dir_input)
+
+with torch.no_grad():
+	for file in files:
+		inp = Image.open(root_dir_input + file)
+		trans = torchvision.transforms.ToTensor()
+		trans1 = torchvision.transforms.ToPILImage()
+		tensor = 2.0*(trans(inp)-0.5).to(device)
+		tensor = tensor.view(1,tensor.shape[0],tensor.shape[1],tensor.shape[2])
+		output = ((generator(tensor)/2.0)+0.5)
+		tensor = ((tensor/2.0)+0.5)
+		output = output.view(output.shape[1],output.shape[2],output.shape[3])
+		
+		output_image = trans1(output)
+
+		# output_image.show()
+		output_image.save(root_dir_output+file)
+		print("Image %s done" % (file))
