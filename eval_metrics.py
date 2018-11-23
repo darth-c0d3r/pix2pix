2b1a3ab6598aeb180a4f7e2e0aff8db0706ad556
rathi22 <rajatrathidgr81@gmail.com>
2018-11-23 14:39:33 +0530

    Updated metrics

--- eval_metrics.py
+++ eval_metrics.py
@@ -3,9 +3,9 @@ from PIL import Image
 import numpy as np
 from skimage.measure import compare_ssim as ssim
 
-def RootMeanSquareDifference(image_out, image_ref):
+def RootMeanSquareDeviation(image_out, image_ref):
 	assert image_out.shape == image_ref.shape, "Eval error: Image Size mismatch"
-	return np.linalg.norm(image_out - image_ref)/(255.0*image_ref.size)
+	return np.linalg.norm(image_out - image_ref)/(255.0*(image_ref.size**0.5))
 
 def StructuralSimilarityIndex(image_out, image_ref):
 	assert image_out.shape == image_ref.shape, "Eval error: Image Size mismatch"
@@ -18,7 +18,7 @@ for idx in range(len(os.listdir(root_dir+'/target/'))):
 	img1 = np.asarray(Image.open(root_dir+'/output/'+str(idx+1)+'.jpg'))
 	img2 = np.asarray(Image.open(root_dir+'/target/'+str(idx+1)+'.jpg'))
 	ssim_list.append(StructuralSimilarityIndex(img1,img2))
-	rmsd_list.append(RootMeanSquareDifference(img1,img2))
+	rmsd_list.append(RootMeanSquareDeviation(img1,img2))
 
 print('Avg. SSIM: '+ str(np.mean(ssim_list)))
 print('Avg. RMSD: '+ str(np.mean(rmsd_list)))
